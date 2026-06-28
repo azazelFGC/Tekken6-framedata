bdd8bb9e75e8efdc67b45ca43bbd4f72e98aa241
jeova <jeovasantos192@gmail.com>
2026-06-28 13:40:35 -0300

    fix: Renders multiple tables

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ import { setupCounter } from "./counter.js";
 // import framedatas from "./data/framedata.json";
 const mainNav = document.querySelector("#main-nav");
 
+const slugify = (text) => {
+  return text
+    .toString()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9 -]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+};
 const getIndexes = async () => {
   try {
     const response = await fetch(`${import.meta.env.BASE_URL}data/index.json`);
@@ -58,89 +69,80 @@ const renderFrameData = (characterData) => {
     const div = document.createElement("div");
     const h1 = document.createElement("h1");
     h1.innerText = "TESTE";
-    div.classList.add("w-full");
+
+    div.className = "max-h-full flex flex-col gap-2 h-fjull  w-full";
 
     div.innerHTML = ` 
-              <h1 class="text-gray-100 ">${action.action} </h1>
-              <div id=${action.action}
-                class="max-w-full h-full max-h-130 sm:max-h-200 bg-gray-950/90   flex bg-gradient-to-r from-gray-950/80 from-20% to-black/50 "
+              <h1 class="h-min  text-gray-100 ">${action.action} </h1>
+              <div id=${slugify(action.action)}
+                class="max-w-full h-95/100 max-h-d130 sm:max-h-200 bg-gray-950/90 flejx bg-gradient-to-r from-gray-950/80 from-20% to-black/50 "
               >
                
-                <div class="tefst h-full overflow-x-auto bg-gradient-to-r from-gray-950 from-20% to-pink-900/0  max-w-full ">
+                <div class="h-full   overflow-x-auto bg-gradient-to-r from-gray-950 from-20% to-pink-900/0    max-w-full ">
                   <table
-                    class="table-auto  sm:w-200 bordder-y-1 border-t-pink-950 border-b-pink-950"
+                    class=" table-auto sm:w-200 bordder-y-1 border-t-pink-950 border-b-pink-950"
                   >
                     <thead class="text-gray-300      bordjer-x-pink-800 bordder-r-1">
                       <tr class="">
-                        <th class="p-2 sticky top-0 ">
+                        <th class="p-2 sticky top-0  p-2 border-x-pink-950/70 border-x-1">
                           <div class="flex w-full">
                             <p>INPUT</p>
                           </div>
                         </th>
-                        <th class="p-2 sticky top-0">
+                        <th class="p-2 sticky top-0 p-2 border-x-pink-950/70 border-x-1">
                           <div class="flex w-full">
                             <p>RANGE</p>
                           </div>
                         </th>
-                        <th class="p-2 sticky top-0">
+                        <th class="p-2 sticky top-0 p-2 border-x-pink-950/70 border-x-1">
                           <div class="flex w-full">
                             <p>DMG</p>
                           </div>
                         </th>
 
-                        <th class="p-2 sticky top-0">
+                        <th class="p-2 sticky top-0 p-2 border-x-pink-950/70 border-x-1">
                           <div class="flex w-full">
                             <p>SPEED</p>
                           </div>
                         </th>
 
-                        <th class="p-2 sticky top-0">
+                        <th class="p-2 sticky top-0 p-2 border-x-pink-950/70 border-x-1">
                           <div class="flex w-full">
                             <p>BLOCK</p>
                           </div>
                         </th>
 
-                        <th class="p-2 sticky top-0">
+                        <th class="p-2 sticky top-0 p-2 border-x-pink-950/70 border-x-1">
                           <div class="flex w-full">
                             <p>HIT</p>
                           </div>
                         </th>
 
-                        <th class="p-2 sticky top-0">
+                        <th class="p-2 sticky top-0 p-2 border-x-pink-950/70 border-x-1">
                           <div class="flex w-full">
                             <p>CH</p>
                           </div>
                         </th>
                       </tr>
                     </thead>
-                    <tbody id=${"main-body-" + action.action} class="text-gray-500" > </tbody>
+                    <tbody id=${"main-body-" + slugify(action.action)} class="text-gray-500" > </tbody>
                   </table>
                 </div></div>`;
+
     target.append(div);
-    // const mobileBody = document.querySelector("#mobile-body-" + action.action);
-    // mobileBody.innerHTML = "";
-    const mainBody = document.querySelector("#main-body-" + action.action);
+
+    const mainBody = document.querySelector(
+      "#main-body-" + slugify(action.action),
+    );
     mainBody.innerHTML = "";
     action.data.forEach((input) => {
-      const renderMobileTr = () => {
-        const tr = document.createElement("tr");
-        const td = document.createElement("td");
-        td.innerText = input.input;
-        td.className = "p-2";
-        tr.className =
-          "border-t-1 text-nowrap border-t-pink-950/20 hover:border-l-pink-950/20 border-x-gray-800/0 border-x-1 hover:bg-gradient-to-r from-pink-950/30 to-pink-950/70";
-        tr.append(td);
-        mobileBody.append(tr);
-      };
       const renderMainTr = () => {
         const tr = document.createElement("tr");
-        const fragment = document.createDocumentFragment();
         ["input", "range", "DMG", "speed", "block", "hit", "ch"].forEach(
           (data) => {
             const td = document.createElement("td");
             td.innerText = input[data];
-            td.className =
-              data === "input" ? "p-2 " : "p-2 border-x-pink-950/70 border-x-1";
+            td.className = "p-2 border-x-pink-950/70 border-x-1";
             tr.append(td);
             tr.onclick = () => {
               // const search = new Array(
@@ -152,12 +154,13 @@ const renderFrameData = (characterData) => {
               //   }
               // });
               tr.classList.toggle("bg-gradient-to-r");
+              tr.classList.toggle("text-white/80");
             };
           },
         );
 
         tr.className =
-          "border-t-1 text-nowrap border-t-pink-950/45 hover:border-l-pink-950/20 border-x-pink-900/50 border-r-1 hover:bg-gradient-to-r from-pink-950/30 to-pink-950/70";
+          "border-t-1 cursor-pointer text-nowrap border-t-pink-950/45 hover:border-l-pink-950/20 border-x-pink-900/50 border-r-1 hover:bg-gradient-to-r from-pink-950/90 to-pink-950/70";
         mainBody.append(tr);
       };
       // renderMobileTr();
@@ -165,23 +168,28 @@ const renderFrameData = (characterData) => {
     });
   });
 };
+const addCurrentAStyle = (currentA) => {
+  currentA.classList.add("sm:bg-gradient-to-l");
+  currentA.classList.add("text-white");
+  currentA.classList.add("font-black");
+  currentA.classList.add("text-lg");
+};
 window.addEventListener("hashchange", async () => {
   console.log("TESTEt");
   try {
-    document
-      .querySelector(`a.sm\\:bg-gradient-to-l`)
-      .classList.toggle("border-b-1");
-    document
-      .querySelector(`a.sm\\:bg-gradient-to-l`)
-      .classList.toggle("sm:bg-gradient-to-l");
+    const pastA = document.querySelector(`a.sm\\:bg-gradient-to-l`);
+
+    pastA.classList.toggle("text-lg");
+    pastA.classList.toggle("text-white");
+    pastA.classList.toggle("font-black");
+    pastA.classList.toggle("sm:bg-gradient-to-l");
   } catch (err) {}
   try {
     const currentA = document.querySelector(
       `nav a[href="${window.location.hash}"]`,
     );
     if (currentA) {
-      currentA.classList.add("sm:bg-gradient-to-l");
-      currentA.classList.add("border-b-1");
+      addCurrentAStyle(currentA);
     }
 
     const character = window.location.hash.substring(1);
@@ -196,7 +204,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     indexes.chars.forEach((value) => {
       const a = document.createElement("a");
       a.className =
-        "text-text text-nowrap hover:bg-pink-600/20 hover:text-100 border-b-pink-700 p-4 from-pink-900/30 to-pink-900/70";
+        " px-2 text-nowrap sm:w-full hover:bg-pink-600/20 hover:text-100 border-b-pink-700 bg-rged-600  sm:p-4 from-pink-900/30 to-pink-900/70";
       a.innerText = value;
       a.href = "#" + value;
       mainNav.append(a);
@@ -206,8 +214,7 @@ window.addEventListener("DOMContentLoaded", async () => {
         `nav a[href="${window.location.hash}"]`,
       );
       if (currentA) {
-        currentA.classList.add("sm:bg-gradient-to-l");
-        currentA.classList.add("border-b-1");
+        addCurrentAStyle(currentA);
       }
       const character = window.location.hash.substring(1);
 
